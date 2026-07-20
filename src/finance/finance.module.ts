b7c4e554d91ac5a21e69d149416ee8fc6d0bb7fd
Antonio Andriatsiaforitrarivo <tonioandria2@gmail.com>
2026-07-20 13:40:02 +0300

    feat(finance): register Parent entity in module

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -7,6 +7,7 @@ import { Facture } from './entities/facture.entity';
 import { Paiement } from './entities/paiement.entity';
 import { Depense } from './entities/depense.entity';
 import { Etudiant } from '../etudiant/entities/etudiant.entity';
+import { Parent } from '../parent/entities/parent.entity';
 import { Classe } from '../classe/entities/classe.entity';
 import { Niveau } from '../niveau/entities/niveau.entity';
 import { AnneeUniversitaireModule } from '../annee-universitaire/annee-universitaire.module';
@@ -19,6 +20,7 @@ import { AnneeUniversitaireModule } from '../annee-universitaire/annee-universit
       Paiement,
       Depense,
       Etudiant,
+      Parent,
       Classe,
       Niveau,
     ]),
